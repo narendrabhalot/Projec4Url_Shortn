bfa448dc085241f5e6fa9211d3dd84490259ee32
Niladri Sekhar Mukherjee <niladrisekharmukherjee3199@gmail.com>
2022-05-17 13:04:18 +0530

    ready to proceed

---
+++ src/index.js
@@ -0,0 +1,26 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const mongoose = require('mongoose')
+const app = express()
+const port = 3000
+const route = require('./routes/route');
+
+
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+
+mongoose.connect("mongodb+srv:REYNIL310609:OnIYmcfVuOkV0Dkr@cluster0.csvzw.mongodb.net/Group102Database?retryWrites=true&w=majority",
+{
+    useNewUrlParser: true
+})
+
+    .then(() => console.log("MongoDB is Connected."))
+    .catch((err) => console.log(err.message))
+
+app.use('/', route);
+
+
+app.listen(process.env.PORT || port, function () {
+    console.log("Express app is running on ", process.env.PORT || port)
+})
